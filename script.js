dc737e846d712c167f2a43cd2f2527727fd3c210
anh s <jimmianh1807@gmail.com>
2022-08-18 15:46:37 +0700

    frist push

---
+++ script.js
@@ -0,0 +1,13 @@
+var love = document.getElementById("love");
+var no_love =document.getElementById("no-love");
+
+love.addEventListener("click", myFunction);
+function myFunction(){
+    alert('Không nhận thì thôi')
+}
+
+no_love.addEventListener("mouseover",lovefunction);
+function lovefunction(){
+    no_love.classList.toggle('move')
+}
+
